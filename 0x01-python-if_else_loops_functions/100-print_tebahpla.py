2bf18d425b450d78c4f37f8b8a89b33cdd711029
ifechukwu <125530349+coditzeey@users.noreply.github.com>
2024-03-12 19:56:34 +0100

    Update 100-print_tebahpla.py

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
-for i in range(ord('z'), ord('a')-1, -1):
-    print('{:c}'.format(i) if i % 2 == 0 else chr(i-32), end='')
+for i in range(122, 96, -1):
+    char = i
+    if i % 2 != 0:
+        char = char - 32
+    print("{:c}".format(char), end='')
